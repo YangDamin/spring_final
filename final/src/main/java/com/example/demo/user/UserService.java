4900907fdebe92356c9390bf589db3dce9eec6a6
YangDamin <cxz26@naver.com>
2022-02-26 18:09:45 +0900

    User 테이블 생성

---
+++ final/src/main/java/com/example/demo/user/UserService.java
@@ -0,0 +1,45 @@
+package com.example.demo.user;
+
+import org.springframework.stereotype.Service;
+
+import lombok.AllArgsConstructor;
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+@Service
+@AllArgsConstructor
+public class UserService {
+    
+    private final UserRepository userRepository;
+
+    // 전체 유저 목록 조회
+    @Transactional
+    public List<User> getUsers(){
+        return userRepository.findAll();
+    }
+
+    // 유저 상세 조회
+    @Transactional
+    public User getUser(Long id){
+        return userRepository.findById(id).get();
+    }
+
+    // 유저 등록
+    @Transactional
+    public User saveUser(User user){
+        return userRepository.save(user);
+    }
+
+    // 유저 수정
+    @Transactional
+    public User modifyUser(User user){
+        return userRepository.save(user);
+    }
+
+    // 유저 삭제
+    @Transactional
+    public void deleteUser(Long id){
+        userRepository.deleteById(id);
+    }
+}
