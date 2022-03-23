0ef87e7ef71644c222abe94aa050692ed45b8a57
haeyoungim <wwkoi@naver.com>
2022-03-23 23:21:37 +0900

    카카오 api

---
+++ final/src/main/java/com/example/demo/controller/OAuthController.java
@@ -0,0 +1,82 @@
+// package com.example.demo.controller;
+
+// import java.io.BufferedReader;
+// import java.io.IOException;
+// import java.io.InputStreamReader;
+// import java.net.HttpURLConnection;
+// import java.net.URL;
+
+// import com.google.gson.JsonElement;
+// import com.google.gson.JsonParser;
+
+// import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.web.bind.annotation.RequestParam;
+// import org.springframework.web.bind.annotation.ResponseBody;
+// import org.springframework.web.bind.annotation.RestController;
+
+// import lombok.AllArgsConstructor;
+
+// @RestController
+// @AllArgsConstructor
+// @RequestMapping("/oauth")
+// public class OAuthController {
+
+//     /**
+//      * 카카오 callback
+//      * [GET] /oauth/kakao/callback
+//      */
+//     @ResponseBody
+//     @GetMapping("/kakao")
+//     public void kakaoCallback(@RequestParam String code) {
+//         System.out.println(code);
+//     }
+
+//     public void createKakaoUser(String token) {
+
+//         String reqURL = "https://kapi.kakao.com/v2/user/me";
+    
+//         //access_token을 이용하여 사용자 정보 조회
+//         try {
+//            URL url = new URL(reqURL);
+//            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+    
+//            conn.setRequestMethod("POST");
+//            conn.setDoOutput(true);
+//            conn.setRequestProperty("Authorization", "Bearer " + token); //전송할 header 작성, access_token전송
+    
+//            //결과 코드가 200이라면 성공
+//            int responseCode = conn.getResponseCode();
+//            System.out.println("responseCode : " + responseCode);
+    
+//            //요청을 통해 얻은 JSON타입의 Response 메세지 읽어오기
+//            BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+//            String line = "";
+//            String result = "";
+    
+//            while ((line = br.readLine()) != null) {
+//                result += line;
+//            }
+//            System.out.println("response body : " + result);
+    
+//            //Gson 라이브러리로 JSON파싱
+//            JsonParser parser = new JsonParser();
+//            JsonElement element = parser.parse(result);
+    
+//            int id = element.getAsJsonObject().get("id").getAsInt();
+//            boolean hasEmail = element.getAsJsonObject().get("kakao_account").getAsJsonObject().get("has_email").getAsBoolean();
+//            String email = "";
+//            if(hasEmail){
+//                email = element.getAsJsonObject().get("kakao_account").getAsJsonObject().get("email").getAsString();
+//            }
+    
+//            System.out.println("id : " + id);
+//            System.out.println("email : " + email);
+    
+//            br.close();
+    
+//            } catch (IOException e) {
+//                 e.printStackTrace();
+//            }
+//      }
+// }
--- final/src/main/java/com/example/demo/controller/UserController.java
+++ final/src/main/java/com/example/demo/controller/UserController.java
@@ -3,6 +3,7 @@ package com.example.demo.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,21 +13,30 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import com.example.demo.model.User;
 import com.example.demo.repository.UserRepository;
 import com.example.demo.service.KakaoAPI;
 import com.example.demo.service.UserService;
+import com.github.scribejava.core.model.OAuth2AccessToken;
 
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 @Controller
 @CrossOrigin(origins = "http://localhost:3000")
 public class UserController {
@@ -132,41 +142,120 @@ public class UserController {
 
 // ========================== Kakao Login ==============================
 
-
+// @RequestMapping(value="/kakaoLogin", method=RequestMethod.GET)
+// public String kakaoLogin(@RequestParam(value = "code", required = false) String code) throws Exception {
+//     System.out.println("#########" + code);
+//     return "member/testPage";
+//     /*
+//      * 리턴값의 testPage는 아무 페이지로 대체해도 괜찮습니다.
+//      * 없는 페이지를 넣어도 무방합니다.
+//      * 404가 떠도 제일 중요한건 #########인증코드 가 잘 출력이 되는지가 중요하므로 너무 신경 안쓰셔도 됩니다.
+//      */
+//     }
+    
 //카카오 로그인
-KakaoAPI kakaoApi = new KakaoAPI();
+// KakaoAPI kakaoApi = new KakaoAPI();
 
-@RequestMapping(value = "/login")
-public ModelAndView login(@RequestParam("code") String code, HttpSession session) {
-    ModelAndView mav = new ModelAndView();
-    // 1번 인증코드 요청 전달
-    String accessToken = kakaoApi.getAccessToken(code);
-    // 2번 인증코드로 토큰 전달
-    HashMap<String, Object> userInfo = kakaoApi.getUserInfo(accessToken);
+// @RequestMapping(value = "/oauth/callback/kakao")
+// public ModelAndView login(@RequestParam("code") String code, HttpSession session) {
+//     ModelAndView mav = new ModelAndView();
+//     // 1번 인증코드 요청 전달
+//     String accessToken = kakaoApi.getKakaoAccessToken(code);
+//     // 2번 인증코드로 토큰 전달
+//     HashMap<String, Object> userInfo = kakaoApi.getUserInfo(accessToken);
+//     System.out.println("카카오 로그인");
+//     System.out.println("login info : " + userInfo.toString());
 
-    System.out.println("login info : " + userInfo.toString());
-
-    if (userInfo.get("email") != null) {
-        session.setAttribute("userId", userInfo.get("email"));
-        session.setAttribute("accessToken", accessToken);
-    }
-    mav.addObject("userId", userInfo.get("email"));
-    mav.setViewName("index");
-    return mav;
-}
+//     if (userInfo.get("email") != null) {
+//         session.setAttribute("userId", userInfo.get("email"));
+//         session.setAttribute("accessToken", accessToken);
+//     }
+//     mav.addObject("userId", userInfo.get("email"));
+//     mav.setViewName("index");
+//     return mav;
+// }
 
 
 //카카오 로그아웃
-@RequestMapping(value = "/logout")
-public ModelAndView logout(HttpSession session) {
-    ModelAndView mav = new ModelAndView();
+// @RequestMapping(value = "/logout")
+// public ModelAndView logout(HttpSession session) {
+//     ModelAndView mav = new ModelAndView();
 
-    kakaoApi.kakaoLogout((String) session.getAttribute("accessToken"));
-    session.removeAttribute("accessToken");
-    session.removeAttribute("userId");
-    mav.setViewName("index");
-    return mav;
+//     kakaoApi.kakaoLogout((String) session.getAttribute("accessToken"));
+//     session.removeAttribute("accessToken");
+//     session.removeAttribute("userId");
+//     mav.setViewName("index");
+//     return mav;
+// }
+
+// @RequestMapping(value="/oauth/kakao", method=RequestMethod.GET)
+// public String kakaoLogin(@RequestParam(value = "code", required = false) String code) throws Exception {
+//     System.out.println("#########" + code);
+//     String access_Token = userService.getAccessToken(code);
+//     HashMap<String, Object> userInfo = userService.getUserInfo(access_Token);
+//     System.out.println("###access_Token#### : " + access_Token);
+//     System.out.println("###nickname#### : " + userInfo.get("nickname"));
+//     System.out.println("###email#### : " + userInfo.get("email"));
+//     return "redirect:/";
+//     }
+
+
+	@RequestMapping(value="/oauth/kakao", method=RequestMethod.GET)
+	public String kakaoLogin(@RequestParam(value = "code", required = false) String code) throws Exception {
+		System.out.println("#########" + code);
+        
+		// 위에서 만든 코드 아래에 코드 추가
+		String access_Token = userService.getAccessToken(code);
+		System.out.println("###access_Token#### : " + access_Token);
+        
+		return "/";
+    	}
+
+//     @Controller
+// public class NaverLogin{
+//     private static final Log LOG = LogFactory.getLog(NaverLogin.class );
+
+//     @RequestMapping(value = "/nearo")
+//     public String initLogin(Model model, HttpSession session, NaverLoginBO naverLoginBO) throws Exception {
+
+//         String naverAuthUrl = naverLoginBO.getAuthorizationUrl(session);
+//         LOG.info("네이버 url : "+naverAuthUrl);
+//         /* 생성한 인증 URL을 View로 전달 */
+//         model.addAttribute("naver_url", naverAuthUrl);
+
+//         /* 생성한 인증 URL을 Model에 담아서 전달 */
+//         return "nearo";
+//     }
+
+//     @RequestMapping(value = "/")
+//     public String getUserProfile(
+//             RedirectAttributes redirectAttributes, Model model,
+//             HttpSession session, HttpServletRequest request,
+//             HttpServletResponse response, NaverLoginBO naverLoginBO) throws IOException {
+//         //파라메터의 값을 세션에 저장
+//         String state = request.getParameter("state");
+//         String code = request.getParameter("code");
+//         LOG.info("코드, 상태 확인 : "+ state+" , " +code);
+//         //리다이렉트로 이 메소드가 다시 실행되므로 세션영역에 getParameter의 null값이 들어가지 않도록 보호
+//         if(state!=null && code!=null) {
+//             session.setAttribute("state", state);
+//             session.setAttribute("code", code);
+//         }
+//         LOG.info("세션 값 확인 : "+ session.getAttribute("state")+"  "+ session.getAttribute("code"));
+
+//         /* getParameter로 값을 주는 경우 code와 state로 인증토큰(oauthToken)을 획득한 뒤 세션에 저장
+//         * 나중에 다른 곳에서 정보가 필요해지면 session에 저장된 토큰을 사용하여
+//         REST-API 방식으로 정보를 요청할 것임.
+//         * 성공적으로 인증과 발급이 완료되면 addFlashAttribute로 전달할 값을 저장하고
+//         리다이렉트로 현재 메소드를 다시 실행하여 메인 페이지를 전송*/
+
+//         OAuth2AccessToken oauthToken = naverLoginBO.getAccessToken(session, code, state);
+//         session.setAttribute("oauthToken",oauthToken);
+//         String apiResult = naverLoginBO.getUserProfile(oauthToken);
+//         System.out.println("Naver login success");
+//         session.setAttribute("result",apiResult);
+//         return "redirect:http://13.124.70.197:8080/base";
+//     }
+    
 }
 
-
-}
