f2071ff80cece506310c1e10b27757ef72b56d24
YangDamin <cxz26@naver.com>
2022-03-31 16:51:05 +0900

    비밀번호 암호화, 로그인, 비밀번호 변경, 비밀번호 찾기 기능 추가

--- final/src/main/java/com/example/demo/controller/UserController.java
+++ final/src/main/java/com/example/demo/controller/UserController.java
@@ -2,6 +2,8 @@ package com.example.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -40,6 +42,9 @@ public class UserController {
     @Autowired
     HttpSession session;
 
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
     // 전체 유저 목록 조회
     @GetMapping("/users")
     public List<User> getUsers() {
@@ -59,16 +64,30 @@ public class UserController {
 
         Map<String, Object> result = new HashMap<>();
         User dbUser = userRepository.findByEmail(user.getEmail());
+        System.out.println(dbUser);
+        User findUser = userRepository.findByNameAndPhone(user.getName(), user.getPhone());
+        System.out.println(findUser);
 
-        if (dbUser != null) { // DB에 user가 있으면
+        //비밀번호 인코딩
+        
+        System.out.println("비번 인코딩"+passwordEncoder.encode(user.getPwd()));
+        
+
+        if (dbUser != null) { // DB에 이메일이 있으면
             System.out.println("회원가입 실패");
             result.put("msg", "🤦‍♂️회원가입 실패🤦‍♂️");
             result.put("code", 201);
-        } else { // DB에 user가 없으면
-            userRepository.save(user);
-            System.out.println("회원가입 성공");
-            result.put("msg", "👊회원가입 성공👊");
-            result.put("code", 200);
+        } else { // DB에 이메일이 없으면
+            if(findUser != null){   // DB에 해당 회원정보가 있으면
+                System.out.println("이미 가입된 회원정보입니다.");
+                result.put("code",401);
+            }else{
+                user.setPwd(passwordEncoder.encode(user.getPwd()));
+                userRepository.save(user);
+                System.out.println("회원가입 성공");
+                result.put("msg", "👊회원가입 성공👊");
+                result.put("code", 200);
+            }
         }
         return result;
     }
@@ -76,18 +95,25 @@ public class UserController {
     // 비밀번호 변경
     @PostMapping("/mypage")
     @ResponseBody
-    public String modifyPw(@ModelAttribute User user) {
+    public Map<String, Object> modifyPw(@ModelAttribute User user) {
+        Map<String, Object> result = new HashMap<>();
         User modifyUser = userRepository.findByEmail(user.getEmail());
+        boolean pwdMatch = passwordEncoder.matches(user.getPwd(), modifyUser.getPwd());
 
-        if (modifyUser != null) {
-            modifyUser.setPwd(user.getPwd());
+
+        if (modifyUser != null && pwdMatch == false) {   // 기존 비번과 새로운 비번이 일치하지않으면 비번변경
+            modifyUser.setPwd(passwordEncoder.encode(user.getPwd()));
             userRepository.save(modifyUser);
+            result.put("compare", pwdMatch);
+            result.put("pwd", modifyUser.getPwd());
+        }else {
+            result.put("compare", pwdMatch);
         }
         System.out.println("비밀번호 : " + user.getPwd());
         System.out.println("이메일 : " + user.getEmail());
         System.out.println(modifyUser);
 
-        return user.getPwd();
+        return result;
     }
 
     // 유저 수정
@@ -135,15 +161,62 @@ public class UserController {
         return result;
     }
 
+    // 임시 비밀번호 발급
+    public String getRandomPassword(int len){
+        char[] charSet = new char[] {
+            '0', '1', '2', '3', '4', '5', '6', '7', '8',
+             '9', 
+             'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 
+             'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+            't', 'u', 'v', 'w', 'x', 'y', 'z'
+        };
+        int idx = 0;
+        StringBuffer sb = new StringBuffer();
+        for(int i=0; i<len; i++){
+            idx = (int) (charSet.length * Math.random());
+            sb.append(charSet[idx]);
+        }
+        System.out.println("임시 비번 : "+ sb.toString());
+        return sb.toString();
+    }
+
+    @PostMapping("/users/findPassword")
+    @ResponseBody
+    public Map<String, Object> findPW(@ModelAttribute User user) {
+        Map<String, Object> result = new HashMap<>();
+
+        User findUser = userRepository.findByEmailAndNameAndPhone(user.getEmail(), user.getName(), user.getPhone());
+        System.out.println(findUser);
+
+        //임시 비번
+        getRandomPassword(10);
+
+        if (findUser != null) { // 이메일,이름, 휴대폰번호에 해당하는 유저가 있으면
+            System.out.println("해당 계정에 임시 비번 발급!");
+            
+            findUser.setPwd(getRandomPassword(10));
+            userRepository.save(findUser);
+
+            result.put("findPwd", findUser.getPwd());
+            result.put("code", 200);
+        } else {
+            System.out.println("이메일+이름+번호에 해당하는 이메일이 없다.");
+            result.put("code", 400);
+        }
+        return result;
+    }
+    
+    
     // 로그인
     @PostMapping("/users/signin")
     @ResponseBody
     public Map<String, Object> login(@ModelAttribute User user) {
         Map<String, Object> result = new HashMap<>();
-        User loginUser = userRepository.findByEmailAndPwd(user.getEmail(), user.getPwd());
-        System.out.println(user.getEmail() + "\n" + user.getPwd());
+        User loginUser = userRepository.findByEmail(user.getEmail());
+        boolean pwdMatch = passwordEncoder.matches(user.getPwd(), loginUser.getPwd());
+        System.out.println("비번 일치하는지?"+pwdMatch);
 
-        if (loginUser != null) { // DB에 user가 있으면
+        if ((loginUser != null && pwdMatch == true) | (loginUser != null && (user.getPwd().equals(loginUser.getPwd()) ))) { // DB에 user가 있으면
             session.setAttribute("user_info", loginUser);
             System.out.println("로그인 성공");
             result.put("user", loginUser);
--- final/src/main/java/com/example/demo/repository/UserRepository.java
+++ final/src/main/java/com/example/demo/repository/UserRepository.java
@@ -7,6 +7,7 @@ import com.example.demo.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long>{
+    public User findByEmailAndNameAndPhone(String email, String name, String phone);
     public User findByEmail(String email);
     public User findByEmailAndPwd(String email, String pwd);
     public User findByNameAndPhone(String name, String phone);
